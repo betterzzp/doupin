df2688439fe64e7409c1a02ad96545ced9f9141e
betterzzp <2379759974@qq.com>
2020-04-18 17:30:16 +0800

    删除代码

--- users/src/main/java/com/zhouzhongping/cn/mapper/ShopcarMapper.java
+++
@@ -1,7 +0,0 @@
-package com.zhouzhongping.cn.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.zhouzhongping.cn.entity.Shopcar;
-
-public interface ShopcarMapper extends BaseMapper<Shopcar> {
-}
--- users/src/main/java/com/zhouzhongping/cn/util/UserAddDetail.java
+++
@@ -1,5 +0,0 @@
-package com.zhouzhongping.cn.util;
-
-public class UserAddDetail {
-
-}
